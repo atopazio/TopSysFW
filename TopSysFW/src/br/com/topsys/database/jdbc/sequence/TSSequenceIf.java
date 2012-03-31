2ce443d46f075257ac0b27292e951a95aca7a7de
andreluismps <andreluismps@gmail.com>
2012-03-31 09:31:10 -0300

    Removendo pasta br e jar

--- TopSysFW/src/br/com/topsys/database/jdbc/sequence/TSSequenceIf.java
+++
@@ -1,7 +0,0 @@
-package br.com.topsys.database.jdbc.sequence;
-
-public interface TSSequenceIf {
-	public Long getNextValue(String nomeSequence);
-	public Long getCurrentValue(String nomeSequence);
-	
-}
